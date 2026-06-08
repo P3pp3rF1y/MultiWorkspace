2d4394ebd13f102f4bb35c87f1bdc0a8559219f6
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-08 12:12:34 +0200

    feat: ✨ Add demo motion automation

--- DevClientAutomation/src/main/java/net/p3pp3rf1y/devclientautomation/DevClientAutomation.java
+++ DevClientAutomation/src/main/java/net/p3pp3rf1y/devclientautomation/DevClientAutomation.java
@@ -55,6 +55,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.network.ClientPacketDistributor;
+import net.p3pp3rf1y.devclientautomation.demo.DemoCommand;
 import net.p3pp3rf1y.devclientautomation.recipeviewer.RecipeViewerAutomationManager;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackBlock;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackBlockEntity;
@@ -121,6 +122,7 @@ public class DevClientAutomation {
 
     public DevClientAutomation(IEventBus modBus) {
         modBus.addListener(DevClientAutomation::clientSetup);
+        DemoCommand.init();
     }
 
     private static void clientSetup(FMLClientSetupEvent event) {
